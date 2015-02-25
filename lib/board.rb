fc488bfccac19b0729df62451a56d1839a628316
danjocutler <danjocutler@gmail.com>
2015-02-25 16:08:34 +0000

    a little more refactoring on the box method

--- lib/board.rb
+++ lib/board.rb
@@ -23,18 +23,18 @@ class Board
 	def box(number) #UGLIEST method ever!!
 		raise "Not a valid number" if number < 1 || number > 9
 		n = number
-		if n < 4
+		if n <= 3
 			@grid.to_a.values_at(((n - 1) * 3)..((n - 1) * 3 + 2), 
 													((n - 1) * 3 + 9)..((n - 1) * 3 + 11),
 													((n - 1) * 3 + 18)..((n - 1) * 3 + 20)).to_h
-		elsif n > 6
-			@grid.to_a.values_at(((n + 11) * 3)..((n + 11) * 3 + 2),
-													((n + 11) * 3 + 9)..((n + 11) * 3 + 11),
-													((n + 11) * 3 + 18)..((n + 11) * 3 + 20)).to_h
-		else
+		elsif n <= 6
 			@grid.to_a.values_at(((n + 5) * 3)..((n + 5) * 3 + 2),
 													((n + 5) * 3 + 9)..((n + 5) * 3 + 11),
 													((n + 5) * 3 + 18)..((n + 5) * 3 + 20)).to_h
+		else
+			@grid.to_a.values_at(((n + 11) * 3)..((n + 11) * 3 + 2),
+													((n + 11) * 3 + 9)..((n + 11) * 3 + 11),
+													((n + 11) * 3 + 18)..((n + 11) * 3 + 20)).to_h
 		end
 	end
 end
